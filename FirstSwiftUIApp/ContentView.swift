67a23148bd2d1ea388467216ad25cbde82837557
annpriya <annapurna.priya@digitas.com>
2019-11-21 12:57:29 +0530

    Initial Commit

---
+++ FirstSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwiftUIApp
+//
+//  Created by annpriya on 21/11/19.
+//  Copyright © 2019 annpriya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
